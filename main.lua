d3f5c6e960a8a2d92efe104b0034f364f6dcbcce
James Donlan <jacketlizard@live.com>
2019-03-25 01:24:12 -0700

    animation runs, loops

--- main.lua
+++ main.lua
@@ -1,3 +1,19 @@
 function love.load()
-  null
+  frames = {}
+  
+  for i=1, 5 do
+    table.insert(frames, love.graphics.newImage("img/jump" .. i .. ".png"))
+  end
+  currentFrame = 1
+end
+
+function love.update(dt)
+  currentFrame = currentFrame + 10 * dt
+  if currentFrame >= 6 then
+    currentFrame = 1
+  end
+end
+
+function love.draw()
+  love.graphics.draw(frames[math.floor(currentFrame)])
 end
